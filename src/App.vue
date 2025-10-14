3039e6926c73e51ff403d4c7a46b0e6ea775fee5
Zoey Kaiser <zoeykaiser8@gmail.com>
2025-10-14 13:25:05 +0200

    init: install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen bg-gray-200 p-10">
+    <h1 class="text-2xl font-semibold">
+      You did it!
+    </h1>
+    <p>
+      Visit <a href="https://vuejs.org/" class="underline" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
 </template>
-
-<style scoped></style>
