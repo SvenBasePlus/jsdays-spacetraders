a9d5340b7fec2eb2082f8b410dbcd35b0873b737
Zoey Kaiser <zoeykaiser8@gmail.com>
2025-10-18 12:23:54 +0200

    feat: add new navbar agent info component

---
+++ src/components/Agent/Info.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { useQuery } from '@tanstack/vue-query'
+import { useAgentStore } from '@/stores/agent'
+
+const store = useAgentStore()
+const { data } = useQuery({
+  queryFn: () => store.agentApi.get('/my/agent'),
+  queryKey: ['agent'],
+})
+</script>
+
+<template>
+  <div v-if="data" class="bg-gray-100 rounded py-2 px-4 border-r-8 border-blue-300">
+    <UUser
+      :name="data.data.symbol"
+      :description="data.data.startingFaction"
+    />
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="my-4">
+    <p>
+      Hallo! Willkommen zu SpaceTraders!
+    </p>
+  </div>
+</template>
