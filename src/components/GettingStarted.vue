018a3d1bdbbf22d911715bc8eac580aa0af3747d
Zoey Kaiser <zoeykaiser8@gmail.com>
2025-10-20 10:18:04 +0200

    chore: add getting started information

---
+++ src/components/GettingStarted.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="space-y-4">
+    <h1 class="text-2xl font-semibold">
+      Hallo! Willkommen zu SpaceTraders!
+    </h1>
+    <p>
+      Um ein neues Spiel zu beginnen, musst du ein Konto erstellen und einen <span class="font-semibold">Agenten-Token</span> generieren.
+      Agenten-Tokens werden verwendet, um (fast alle) Anfragen an die API zu authentifizieren, und sind notwendig, um das Spiel zu spielen.
+    </p>
+
+    <h2 class="text-xl font-semibold">
+      Ein Konto erstellen
+    </h2>
+    <p>
+      Um ein Konto zu registrieren, melde dich im SpaceTraders-Dashboard unter <a href="https://my.spacetraders.io" target="_blank" class="underline">https://my.spacetraders.io</a>
+    </p>
+    <p>
+      Nachdem du dich registriert hast, kannst du auf die Seite „<span class="font-semibold">Agents</span>“ gehen und dort einen neuen Agenten anlegen.
+    </p>
+
+    <h2 class="text-xl font-semibold">
+      Links
+    </h2>
+    <ul class="list-disc list-inside">
+      <li>
+        <a href="https://spacetraders.io/" target="_blank" class="underline">
+          SpaceTraders Docs
+        </a>
+      </li>
+      <li>
+        <a href="https://spacetraders.io/openapi" target="_blank" class="underline">
+          SpaceTraders API
+        </a>
+      </li>
+      <li>
+        <a href="https://vuejs.org/guide/introduction.html" target="_blank" class="underline">
+          Vue
+        </a>
+      </li>
+      <li>
+        <a href="https://pinia.vuejs.org/" target="_blank" class="underline">
+          Pinia
+        </a>
+      </li>
+      <li>
+        <a href="https://tanstack.com/query/latest/docs/framework/vue/overview" target="_blank" class="underline">
+          Tanstack Query
+        </a>
+      </li>
+      <li>
+        <a href="https://tailwindcss.com/" target="_blank" class="underline">
+          TailwindCSS
+        </a>
+      </li>
+      <li>
+        <a href="https://ui.nuxt.com/" target="_blank" class="underline">
+          NuxtUI
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
